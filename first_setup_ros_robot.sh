0f42ace39ee83113ad469b4f7fa0d0e633d9d87e
Robert <robert.a.hilton.jr@gmail.com>
2022-01-12 15:44:46 -0500

    Update first_setup_ros_robot.sh

--- first_setup_ros_robot.sh
+++ first_setup_ros_robot.sh
@@ -24,7 +24,10 @@ chmod +x /robot/ros_scripts/*.sh
 
 nmcli con mod "Wired connection 1" ipv4.addresses "10.1.95.5/8" ipv4.gateway "10.0.0.1" ipv4.dns "8.8.8.8,8.8.4.4" ipv4.method "manual" ipv6.method "ignore"
 
+#Disable password entry for sudo
 echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" | (sudo su -c 'EDITOR="tee -a" visudo')
+#Disable USB Autosuspend
+sed -i '$s/$/ usbcore.autosuspend=-1/'  /boot/extlinux/extlinux.conf
 
 #mv /robot/ros_scripts/jetsonclocks.service /etc/systemd/system/
 #chown root:root /etc/systemd/system/jetsonclocks.service
