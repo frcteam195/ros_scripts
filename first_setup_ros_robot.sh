772b10f87b65469c09b3996e66b16c8fbc930bcb
Robert Hilton <robert.a.hilton.jr@gmail.com>
2022-01-14 16:38:37 -0500

    Update scripts

--- first_setup_ros_robot.sh
+++ first_setup_ros_robot.sh
@@ -29,16 +29,11 @@ echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" | (sudo su -c 'EDITOR="tee -a" visudo')
 #Disable USB Autosuspend
 sed -i '$s/$/ usbcore.autosuspend=-1/'  /boot/extlinux/extlinux.conf
 
-#mv /robot/ros_scripts/jetsonclocks.service /etc/systemd/system/
-#chown root:root /etc/systemd/system/jetsonclocks.service
-#chmod 644 /etc/systemd/system/jetsonclocks.service
-
 mv /robot/ros_scripts/robot_run.service /etc/systemd/system/
 chown root:root /etc/systemd/system/robot_run.service
 chmod 644 /etc/systemd/system/robot_run.service
 
 systemctl daemon-reload
-#systemctl enable jetsonclocks.service
 systemctl enable robot_run.service
 
 sudo reboot
--- prestart_robot.sh
+++ prestart_robot.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 source /robot/ros_scripts/robotservice_params.sh
 /bin/sh -c "until ping -c1 ${ROBOT_JETSON_IP}; do sleep 1; done;"
+/usr/bin/jetson_clocks
+
