036e685159198c4628d52edd4196021c719b9280
Robert <robert.a.hilton.jr@gmail.com>
2022-01-22 21:23:05 -0500

    Update unpackros.sh

--- unpackros.sh
+++ unpackros.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo systemctl stop robot_run.service
 rm -Rf /robot/*_Robot
-tar -xzf /robot/rosdeploy.tar.gz --directory /robot/
+tar -mxzf /robot/rosdeploy.tar.gz --directory /robot/
 rm -Rf /robot/rosdeploy.tar.gz
 sudo systemctl start robot_run.service
 
