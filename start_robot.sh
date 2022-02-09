1452ceda2e02b9e690819bd69ea40e4116bcdfb4
Robert <robert.a.hilton.jr@gmail.com>
2022-02-08 19:38:06 -0500

    Update start_robot.sh

--- start_robot.sh
+++ start_robot.sh
@@ -6,6 +6,7 @@ cd /robot
 umask 0002
 docker run --rm \
 	-e ROS_IP=${ROBOT_JETSON_IP} \
+	-e ROS_MASTER_URI="http://${ROBOT_JETSON_IP}:11311" \
 	-v "$(pwd)":/mnt/working \
 	--volume="/etc/group:/etc/group:ro" \
 	--volume="/etc/gshadow:/etc/gshadow:ro" \
