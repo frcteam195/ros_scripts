7a2140bd113ea304231fdc8a736a63b0857a6c44
Robert <robert.a.hilton.jr@gmail.com>
2022-03-18 17:21:56 -0400

    Add rosbag

--- roslaunch_robot.sh
+++ roslaunch_robot.sh
@@ -3,4 +3,5 @@ BASE_PATH=$(find /mnt/working/*_Robot -maxdepth 0 -type d)
 source ${BASE_PATH}/catkin_ws/devel/setup.bash
 LAUNCH_FILE="${BASE_PATH}/launch/prod.launch"
 umask 0002
+rosbag record -a -o /mnt/working &
 roslaunch "${LAUNCH_FILE}"
