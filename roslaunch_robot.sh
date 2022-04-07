5e7d29411c0c74af4a93b5049a96aded7770feef
Robert Hilton <robert.a.hilton.jr@gmail.com>
2022-04-06 21:06:46 -0400

    Update

--- roslaunch_robot.sh
+++ roslaunch_robot.sh
@@ -3,5 +3,6 @@ BASE_PATH=$(find /mnt/working/*_Robot -maxdepth 0 -type d)
 source ${BASE_PATH}/catkin_ws/devel/setup.bash
 LAUNCH_FILE="${BASE_PATH}/launch/prod.launch"
 umask 0002
+/usr/bin/java -jar /mnt/working/ros_scripts/java_service/java_nonlinearff_planner.jar &
 #rosbag record -a -o /mnt/working/ &
 roslaunch "${LAUNCH_FILE}"
--- stop_robot.sh
+++ stop_robot.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #docker container kill $(docker ps -q)
 
+JAVALAUNCH_PID=$(ps aux | grep '/[u]sr/bin/java' | awk '{print $2}')
+sudo kill -s SIGINT ${JAVALAUNCH_PID} || true
 ROSLAUNCH_PID=$(ps aux | grep '/[o]pt/ros/melodic/bin/roslaunch' | awk '{print $2}')
-#sudo pkill rosbag
+#sudo pkill -2 rosbag
 sudo kill -s SIGINT ${ROSLAUNCH_PID}
