24dd58aa2fc15bf027eb37152cb49ccd7f720d24
Robert <robert.a.hilton.jr@gmail.com>
2023-02-11 17:50:21 -0500

    Update stop_robot.sh

--- stop_robot.sh
+++ stop_robot.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #docker container kill $(docker ps -q)
 
-JAVALAUNCH_PID=$(ps aux | grep '/[u]sr/bin/java' | awk '{print $2}')
-sudo kill -s SIGINT ${JAVALAUNCH_PID} || true
 ROSLAUNCH_PID=$(ps aux | grep '/[o]pt/ros/noetic/bin/roslaunch' | awk '{print $2}')
 #sudo pkill -2 rosbag
 sudo kill -s SIGINT ${ROSLAUNCH_PID}
