9b86b4cee73c5114338809a73f7cc9dbe61032c4
Robert Hilton <robert.a.hilton.jr@gmail.com>
2023-02-23 01:39:49 -0500

    Update robotservice_params.sh to test for older device, but default to new

--- robotservice_params.sh
+++ robotservice_params.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-export ROBOT_JETSON_IP=$(sudo cat "/etc/NetworkManager/system-connections/Wired connection 1" | grep address1 | cut -d= -f2 | cut -d/ -f1)
+export ROBOT_JETSON_IP=$(sudo cat "/etc/NetworkManager/system-connections/Wired connection 1.nmconnection" | grep address1 | cut -d= -f2 | cut -d/ -f1)
 
+if [ -z "${ROBOT_JETSON_IP}" ]; then
+    echo "Failed to use default wired connection. Attempting second method for older devices..."
+    export ROBOT_JETSON_IP=$(sudo cat "/etc/NetworkManager/system-connections/Wired connection 1" | grep address1 | cut -d= -f2 | cut -d/ -f1)
+fi
