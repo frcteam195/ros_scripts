f716ac3ba66b1235dcebb9ef44933d510dd47097
Robert <robert.a.hilton.jr@gmail.com>
2023-03-22 20:53:00 -0400

    Update roslaunch_robot.sh

--- roslaunch_robot.sh
+++ roslaunch_robot.sh
@@ -3,4 +3,4 @@ BASE_PATH=$(find /mnt/working/*_Robot -maxdepth 0 -type d)
 source ${BASE_PATH}/catkin_ws/devel/setup.bash
 LAUNCH_FILE="${BASE_PATH}/launch/prod.launch"
 umask 0002
-roslaunch "${LAUNCH_FILE}"
+roslaunch --skip-log-check "${LAUNCH_FILE}"
